7dca85247e8d4d7c6c782ff1217bd04252969633
isveckrali <mehmet.can.seyhan1@gmail.com>
2026-05-10 11:18:54 +0200

    welcome screen done

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,11 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React, { useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
+import { useNavigation } from "@react-navigation/native";
 
 const WelcomeScreen = () => {
+
+  const rinp1padding = useSharedValue(0)
+  const rinp2padding = useSharedValue(0)
+
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    rinp1padding.value = 0
+    rinp2padding.value = 0
+    setTimeout(() => rinp1padding.value = withSpring(rinp1padding.value + hp(5)), 100)
+    setTimeout(() => rinp2padding.value = withSpring(rinp2padding.value + hp(5.5)), 300)
+
+    setTimeout(()=>navigation.navigate('Home',2500))
+
+  }, [])
+
   return (
-    <View className="flex-1 justify-center items-center space-y-10 bg-blue-500">
+    <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
       <StatusBar style='light' />
+
+      {/* logo */}
+      <Animated.View className="bg-white/20 rounded-full" style={{ padding: rinp1padding }}>
+        <Animated.View className="bg-white/20 rounded-full" style={{ padding: rinp2padding }}>
+          <Image
+            source={require("../../assets/images/welcome.webp")}
+            style={{ width: hp(20), height: hp(20) }}
+          />
+        </Animated.View>
+      </Animated.View>
+
+      {/* title */}
+      <View className="flex items-center space-y-2">
+        <Text className="font-bold text-white tracking-widest" style={{ fontSize: hp(7) }}>
+          Udemig
+        </Text>
+        <Text className="font-medium text-white tracking-widest" style={{ padding: hp(2) }}>
+          Yemegin dogru adresi
+        </Text>
+      </View>
     </View>
   )
 }
